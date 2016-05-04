bef2b0bc0aea26b49a730c3079170713d5b3218b
Guilherme Ferreira <guilherme27set@gmail.com>
2016-05-04 00:28:23 -0300

    subindo arquivos

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var pessoas = new Vue({
+    el:'#app',
+    data:{
+        titulo: 'Aprenda Vue com poucas linhas!',
+        lista:[
+            {nome:"Camila",email:"camila@mail.com"},
+            {nome:"Murilo",email:"murilo@mail.com"},
+            {nome:"Pedro",email:"pedro@mail.com"},
+            {nome:"João",email:"joao@mail.com"}
+        ]
+    }
+});
